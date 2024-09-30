84313139b63737ddfdb5910cfd32483955bfeacb
Preetom <167507439+PreetomBoro@users.noreply.github.com>
2024-09-30 15:14:14 +0530

    colors wEEEEEEEEEEEEEE

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 
+def purple(a):
+    return "\33[35m"+a+"\33[0m"
+
 NOTICE_URL = "https://cit.ac.in/pages-notices-all"
 page = requests.get(NOTICE_URL)
 
-# Parse the HTML content of the page using BeautifulSoup
+# Parse the HTML content of the page using 
 soup = BeautifulSoup(page.content, "html.parser")
 
 # Find the table containing the notices (there's only one table on the page)
@@ -14,7 +17,7 @@ table_body = table.find('tbody')
 rows = table_body.find_all('tr')
 
 # Each row represents a single notice
-for row in reversed(rows):
+for row in reversed(rows[0:9]):
     '''
     Each row contains two anchor(<a>) tags:
     - first one contains the actual title of the notice and
@@ -26,4 +29,5 @@ for row in reversed(rows):
     notice_name = anchor_tags[0].text.strip()
     notice_attachment_link = anchor_tags[1]['href']
     
-    print(f'{notice_date} - {notice_name} : {notice_attachment_link}')
+    #print(f'{notice_date} - {notice_name} : {notice_attachment_link}')
+    print(purple(notice_date)+"-"+notice_name+"\n"+notice_attachment_link+"\n")
---
+++ test.py
@@ -0,0 +1,5 @@
+print("\33[35m hello \33[0m")
+
+def purple(a):
+    print("\33[35m"+a+"\33[0m")
+
