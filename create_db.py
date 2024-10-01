6876f7b6f9f67732ae9f0a01dfcf4b78b72fc164
Kinjal Kavish Boro <levihackermangithub@gmail.com>
2024-10-01 19:17:27 +0530

    Add table creation query for subscribers

--- create_db.py
+++ create_db.py
@@ -12,6 +12,12 @@ cursor.execute('''
         link TEXT
     )
 ''')
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS subscribers (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        chat_id TEXT UNIQUE
+    )
+''')
 
 conn.commit()
 conn.close()
